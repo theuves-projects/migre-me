b3ef0dca9aa6abf71b4f5adca9fd6bc99c846e95
Matheus Alves <theuves@outlook.com>
2016-02-11 10:59:27 -0300

    Adiciona index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function (url, cb) {
+    url = 'http://migre.me/api.json?url=' + url;
+    cb = cb || function () {};
+
+    request.get({
+        json: true,
+        url: url
+    }, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            return cb(error || response.message);
+        }
+
+        cb(null, body);
+    });
+}
