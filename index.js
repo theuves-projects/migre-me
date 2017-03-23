00445df02d5220407cfa3525fd6a62cc400b6ec9
Matheus Alves <theuves@outlook.com>
2017-03-23 16:36:01 -0400

    novo estilo

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-'use strict';
+"use strict";
 
-var request = require('request');
+// migre-me | matheus alves
+// https://github.com/theuves/migre-me
+
+var request = require("request");
 
 module.exports = function (url, cb) {
-    url = 'http://migre.me/api.json?url=' + url;
-    cb = cb || function () {
-        return undefined;
-    };
+  url = "http://migre.me/api.json?url=" + url;
+  cb = cb || function () {
+    return undefined;
+  };
 
-    request.get({
-        json: true,
-        url: url
-    }, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            return cb(error || response.message);
-        }
-
-        cb(null, body);
-    });
+  request.get({
+    json: true,
+    url: url
+  }, function (error, response, body) {
+    return (error || response.statusCode !== 200)
+      ? cb(error || response.message)
+      : cb(null, body);
+  });
 };
